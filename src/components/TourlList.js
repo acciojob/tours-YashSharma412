757a0a7e67c88e4eba9955d9ec50d36d089877ca
YashSharma412 <sharmayashsatiish@gmail.com>
2024-01-17 11:55:56 +0530

    Added Loader

--- src/components/TourlList.js
+++ src/components/TourlList.js
@@ -6,27 +6,32 @@ import SingleTour from "./SingleTour";
 function TourlList() {
   
   const [tours, setTours] = useState("");
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
+    setTimeout(()=>setLoading(false), 1000) 
     setTours(tourData);
   }, []);
 
   return (
     <div>
       <h1 className="title">Your Tours</h1>
-      <div className="tours__container">
         {
-            tours.length !== 0 
-            ? tours.map((tour) => (
-                <SingleTour key={tour.id} tour={tour} tours={tours} setTours={setTours}/>
-            ))
-            : <div>
-                <p>
-                    No more tours
-                </p>
-                <button className="btn" onClick={()=>window.location.reload()}>Refresh page</button>
-              </div>
+            loading ? <p>Loading</p> :
+            <div className="tours__container">
+                {
+                    tours.length !== 0 
+                    ? tours.map((tour) => (
+                        <SingleTour key={tour.id} tour={tour} tours={tours} setTours={setTours}/>
+                    ))
+                    : <div>
+                        <p>
+                            No more tours
+                        </p>
+                        <button className="btn" onClick={()=>window.location.reload()}>Refresh page</button>
+                    </div>
+                }
+            </div>
         }
-      </div>
     </div>
   );
 }
